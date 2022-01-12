cd802efb2477585d72b58e4d4c1ca163ed07b293
hyboll <huyangbo3914@163.com>
2022-01-12 22:44:16 +0800

    feat：添加黏包、半包解决方式

--- netty/src/main/java/cn/ovo/learn/netty/demo/EchoServer.java
+++ netty/src/main/java/cn/ovo/learn/netty/demo/EchoServer.java
@@ -6,6 +6,9 @@ import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LineBasedFrameDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
@@ -23,11 +26,21 @@ public class EchoServer {
 
         new ServerBootstrap()
                 .group(boss, workers)
+                // 全局设置
+//                .option(ChannelOption.SO_RCVBUF, 10)
+                // 连接中的设置
+//                .childOption(ChannelOption.RCVBUF_ALLOCATOR, new AdaptiveRecvByteBufAllocator(16,16,16))
                 .channel(NioServerSocketChannel.class)
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
 
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
+                        // 黏包半包解决方式1：定长解码器
+//                        ch.pipeline().addLast(new FixedLengthFrameDecoder(10));
+                        // 黏包半包解决方式2：换行解码器
+//                        ch.pipeline().addLast(new LineBasedFrameDecoder(10));
+                        // 黏包半包解决方式3：TLC (Type Length Content)
+//                        ch.pipeline().addLast(new LengthFieldBasedFrameDecoder(1024,0,4));
                         ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
                         ch.pipeline().addLast(new StringEncoder(StandardCharsets.UTF_8));
                         ch.pipeline().addLast(new ChannelOutboundHandlerAdapter() {
